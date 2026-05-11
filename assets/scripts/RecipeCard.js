b6ea84f7ae09ac48a144e2030ebba6e403c86c33
dlw1rm4 <kaitlyn.loc101@gmail.com>
2026-05-10 21:22:19 -0700

    added A8

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -51,6 +51,7 @@ class RecipeCard extends HTMLElement {
 		if (!data) return;
 
 		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+		
 		// A7. TODO - Set the contents of the <article> with the <article> template given in
 		//           cardTemplate.html and the data passed in (You should only have one <article>,
 		//           do not nest an <article> inside another <article>). You should use template
@@ -62,4 +63,6 @@ class RecipeCard extends HTMLElement {
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
-//           'recipe-card' elements
+//           'recipe-card' elements\
+
+customElements.define('recipe-card', RecipeCard);
